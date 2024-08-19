c90de5729d9d3bb78b9dd3fa32e1207f1110c224
Varshil Desai <varshil.desai@qloudx.com>
2024-08-19 09:25:38 +0530

    updated eks tf code

--- eks/backend.tf
+++ eks/backend.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-remote-state-jenkins"
+    bucket         = "terraform-remote-state-490"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
   #  dynamodb_table = "Lock-Files"
