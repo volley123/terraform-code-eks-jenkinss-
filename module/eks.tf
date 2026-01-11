4afd7f1d4a01c4ccbf25d0971a432509d6e06022
volley123 <desaivarshil5@gmail.com>
2026-01-11 19:08:10 +0530

    Update eks.tf

--- module/eks.tf
+++ module/eks.tf
@@ -35,7 +35,7 @@ resource "aws_eks_addon" "eks-addons" {
   for_each = { for idx, addon in var.addons : idx => addon }
   cluster_name = aws_eks_cluster.eks[0].name
   addon_name   = each.value.name
-  addon_version = try(each.value.version, null)
+  addon_version = each.value.version != "" ? each.value.version : null
 
   depends_on = [
     aws_eks_node_group.ondemand-node,
